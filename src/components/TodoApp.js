92905f765fafa82eb17b54751f4a8e12be516e7a
ko tainen <TonyK@hyvesolutions.com>
2020-03-17 23:16:08 +0800

    add TodoApp.js

---
+++ src/components/TodoApp.js
@@ -0,0 +1,13 @@
+import React from "react";
+import {view as TodoList} from './todos';
+import {view as Filter} from './filter';
+function TodoApp(){
+    return(
+        <div>
+            <TodoList/>
+            <Filter/>
+        </div>
+    )
+};
+
+export default TodoApp;
