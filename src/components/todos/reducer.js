97297842a63e6a41a137e93c83a5b5891b1fb615
ko tainen <TonyK@hyvesolutions.com>
2020-03-17 23:14:29 +0800

    add reducer.js

--- src/components/todos/reducer.js
+++ src/components/todos/reducer.js
@@ -3,11 +3,12 @@ import {ADD_TODO, TOGGLE_TODO, REMOVE_TODO} from "./actionsTypes";
 export default (state = [], action) => {
     switch (action.type) {
         case ADD_TODO: {
-            return [{
-                id: action.id,
-                test: action.text,
-                completed: action.completed
-            },
+            return [
+                {
+                    id: action.id,
+                    text: action.text,
+                    completed: action.completed
+                },
                 ...state
             ]
         }
@@ -15,7 +16,8 @@ export default (state = [], action) => {
             return state.map((todoItem) => {
                 if (todoItem === action.id) {
                     return {
-                        ...todoItem, completed: !todoItem.completed
+                        ...todoItem,
+                        completed: !todoItem.completed
                     };
                 } else {
                     return todoItem;
